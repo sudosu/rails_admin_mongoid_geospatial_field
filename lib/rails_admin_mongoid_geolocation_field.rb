b887f131f6f5c815413b579d7fe9913135cceeed
Oleh <superdodman@gmail.com>
2014-04-22 20:30:30 +0300

    Update and rename rails_admin_mongoid_geolocation_field.rb to rails_admin_mongoid_geospatial_field.rb

--- lib/rails_admin_mongoid_geolocation_field.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_admin'
-require 'rails_admin_mongoid_geolocation_field/rails_admin/config/fields/types/map'
-load 'rails_admin_mongoid_geolocation_field/engine.rb'
-
-# load 'rails_admin_mongoid_geolocation_field/factory.rb'
-
---
+++ lib/rails_admin_mongoid_geospatial_field.rb
@@ -0,0 +1,6 @@
+require 'rails_admin'
+require 'rails_admin_mongoid_geospatial_field/rails_admin/config/fields/types/map'
+load 'rails_admin_mongoid_geospatial_field/engine.rb'
+
+# load 'rails_admin_mongoid_geospatial_field/factory.rb'
+
