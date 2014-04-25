285868da5deefbc5ed485c4787270bddb1c99d22
Oleh <superdodman@gmail.com>
2014-04-25 13:32:18 +0300

    Update rails_admin_mongoid_geospatial_field.rb

--- lib/rails_admin_mongoid_geospatial_field.rb
+++ lib/rails_admin_mongoid_geospatial_field.rb
@@ -1,5 +1,5 @@
 require 'rails_admin'
-require 'rails_admin_mongoid_geospatial_field/rails_admin/config/fields/types/map'
+require 'rails_admin_mongoid_geospatial_field/rails_admin/config/fields/types/geospatial'
 load 'rails_admin_mongoid_geospatial_field/engine.rb'
 
 # load 'rails_admin_mongoid_geospatial_field/factory.rb'
